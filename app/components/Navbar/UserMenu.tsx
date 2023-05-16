4a30e4a2f26c95ff9436f1759a1cf574f33af007
iSatVeerSingh <developer.satveer@gmail.com>
2023-05-16 17:08:43 +0530

    Navbar created

---
+++ app/components/Navbar/UserMenu.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar/Avatar";
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <button className="hidden md:block text-sm font-semibold py-3 px4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+          Airbnb your home
+        </button>
+        <button className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
