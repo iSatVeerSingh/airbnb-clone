ef71905f93eb869a7787035b9cf307b08b5571ab
iSatVeerSingh <developer.satveer@gmail.com>
2023-05-20 08:40:49 +0530

    Signup Created

---
+++ app/components/Models/RegisterModel.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModel from "@/app/hooks/useRegisterModel";
+import { useState } from "react";
+import Model from "./Model";
+import Heading from "../Heading/Heading";
+import Input from "../Inputs/Input";
+import { toast } from "react-hot-toast";
+
+const RegisterModel = () => {
+  const registerModel = useRegisterModel();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+
+    try {
+      await axios.post("/api/register", data);
+      registerModel.onClose();
+    } catch (err) {
+      toast.error('Something went wrong')
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="Email"
+        type="email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  return (
+    <Model
+      disabled={isLoading}
+      isOpen={registerModel.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModel.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModel;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from "next/font/google";
 import { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import RegisterModel from "./components/Models/RegisterModel";
+import ToasterProvider from "./Providers/ToasterProvider";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -15,6 +17,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <ToasterProvider />
+        <RegisterModel />
         <Navbar />
         {children}
       </body>
